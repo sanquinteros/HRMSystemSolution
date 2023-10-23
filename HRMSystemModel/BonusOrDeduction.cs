e3a2bac569543c7033f889168cd9b4526d307ed1
sanquinteros <santiagoqros@gmail.com>
2023-10-23 15:55:57 -0300

    Rename BonusAndDeduction, modify class.

---
+++ HRMSystemModel/BonusOrDeduction.cs
@@ -0,0 +1,17 @@
+﻿namespace HRMSystemModel
+{
+    public class BonusOrDeduction
+    {
+        public int BonusOrDeductionID { get; set; }
+        public int EmployeeID { get; set; }
+        public string Type { get; set; }
+        public bool IsEnabled { get; set; }
+        public DateOnly CreatedDate { get; set; }
+        public decimal Amount { get; set; }
+
+        public override string ToString()
+        {
+            return $"BonusOrDeduction[BonusOrDeductionID='{BonusOrDeductionID}', EmployeeID='{EmployeeID}', Type='{Type}', Type='{IsEnabled}', Type='{CreatedDate}', Amount='{Amount}']";
+        }
+    }
+}
